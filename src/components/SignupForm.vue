83da27ddc79cb0530ee892f4faaa404ba24ca00a
BALDEALFA <alfaafro@gmail.com>
2021-02-28 21:27:43 +0000

    Creating_a_signup_composable

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -14,14 +14,18 @@
 
 <script>
 import { ref } from "vue";
+import useSignup from "../composables/useSignup";
 export default {
 	setup() {
+		const { error, signup } = useSignup();
 		//refs
 		const displayName = ref("");
 		const email = ref("");
 		const password = ref("");
-		const handleSubmit = () => {
-			console.log(displayName.value, email.value, password.value);
+
+		const handleSubmit = async () => {
+			await signup(email.value, password.value, displayName.value);
+			console.log("User signed up");
 		};
 
 		return { displayName, email, password, handleSubmit };
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyArRcOzPrvZ0C8sU9vgGGpF6O4-5ajX588",
@@ -11,10 +12,10 @@ const firebaseConfig = {
 };
 
 // Init firebase
-
 firebase.initializeApp(firebaseConfig);
 
+const projectAuth = firebase.auth();
 const projectFirestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectFirestore, timestamp };
+export { projectAuth, projectFirestore, timestamp };
