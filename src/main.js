e02e77799eee60abe217902e191af9c55e4c0cc1
BALDEALFA <alfaafro@gmail.com>
2021-03-03 11:04:13 +0000

    Waiting_for_Firebase_Auth_to_initialize

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
-createApp(App)
-	.use(router)
-	.mount("#app");
+
+// import firebase auth service
+import { projectAuth } from "./firebase/config";
+
+let app;
+
+projectAuth.onAuthStateChanged(() => {
+	if (!app) {
+		app = createApp(App)
+			.use(router)
+			.mount("#app");
+	}
+});
