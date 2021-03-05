9b4bd23ae7351d25fda51458a1d736c19ab40ba8
BALDEALFA <alfaafro@gmail.com>
2021-03-05 20:34:54 +0000

    Formatting_dates_(date-fns)

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -4,8 +4,8 @@
 			{{ error }}
 		</div>
 		<div v-if="documents" class="messages">
-			<div v-for="doc in documents" :key="doc.id" class="single">
-				<span class="created-at"> {{ doc.createdAt.toDate() }} </span>
+			<div v-for="doc in formattedDocuments" :key="doc.id" class="single">
+				<span class="created-at"> {{ doc.createdAt }} </span>
 				<span class="name"> {{ doc.name }} </span>
 				<span class="message"> {{ doc.message }} </span>
 			</div>
@@ -15,11 +15,21 @@
 
 <script>
 import getCollection from "../composables/getCollection";
+import { formatDistanceToNow } from "date-fns";
+import { computed } from "@vue/runtime-core";
 export default {
 	setup() {
 		const { error, documents } = getCollection("messages");
 
-		return { error, documents };
+		const formattedDocuments = computed(() => {
+			if (documents.value) {
+				return documents.value.map((doc) => {
+					let time = formatDistanceToNow(doc.createdAt.toDate());
+					return { ...doc, createdAt: time };
+				});
+			}
+		});
+		return { error, documents, formattedDocuments };
 	},
 };
 </script>
